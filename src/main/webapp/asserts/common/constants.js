2e86c5e53cb5bbcb196ecbd26edb7535a9294095
wangyao <wangyao@cis.com.cn>
2018-03-23 17:59:21 +0800

    个人简历 待完善

--- src/main/webapp/asserts/common/constants.js
+++ src/main/webapp/asserts/common/constants.js
@@ -1,5 +1,5 @@
 var GLOBAL = {
-		basePath : 'https://www.travelmonk123.com/cloudnote/blog',
-//		basePath : 'http://127.0.0.1:8888/cloudnote/blog',
+//		basePath : 'https://www.travelmonk123.com/cloudnote/blog',
+		basePath : 'http://127.0.0.1:8888/cloudnote/blog',
 		pageSize : 8
 };
