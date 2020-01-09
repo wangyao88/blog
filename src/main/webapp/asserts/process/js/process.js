81c8e4eb4e9ca81773d7fb965f748541320e1c02
wangyao <wangyao@cis.com.cn>
2020-01-09 16:28:39 +0800

    心路历程数据从后台动态查询

--- src/main/webapp/asserts/process/js/process.js
+++ src/main/webapp/asserts/process/js/process.js
@@ -1,5 +1,39 @@
+function getProcess() {
+    $.ajax({
+        url: GLOBAL.basePath + "/getProcess",
+        type: "post",
+        dataType: "json",
+        success: function (result) {
+            var processes = result.data;
+            var event_year = '';
+            var event_list = '';
+            $(processes).each(function (index, process) {
+                if (index === 0) {
+                    event_year += '<li class="current"><label for="' + process.key + '">' + process.key + '</label></li>';
+                } else {
+                    event_year += '<li><label for="' + process.key + '">' + process.key + '</label></li>';
+                }
+                event_list += '<div><h3 id="' + process.key + '">' + process.key + '</h3>' + process.value + '</div>';
+            });
+            $("#event_year").html(event_year);
+            $("#event_list").html(event_list);
+            $('label').click(function () {
+                $('.event_year>li').removeClass('current');
+                $(this).parent('li').addClass('current');
+                var year = $(this).attr('for');
+                $('#' + year).parent().prevAll('div').slideUp(800);
+                $('#' + year).parent().slideDown(800).nextAll('div').slideDown(800);
+            });
+        },
+        error: function () {
+            console.log("获取心路历程失败！")
+        }
+    });
+}
+
 $(document).ready(function () {
     getNavigation();
     getBaseInfo();
     getLink();
+    getProcess();
 });
--- src/main/webapp/asserts/timer/js/timer.js
+++ src/main/webapp/asserts/timer/js/timer.js
@@ -49,16 +49,6 @@ function getRecent(){
 	});
 }
 
-$(function(){
-	$('label').click(function(){
-		$('.event_year>li').removeClass('current');
-		$(this).parent('li').addClass('current');
-		var year = $(this).attr('for');
-		$('#'+year).parent().prevAll('div').slideUp(800);
-		$('#'+year).parent().slideDown(800).nextAll('div').slideDown(800);
-	});
-});
-
 $(document).ready(function(){
 	getRecommend();
 	getRecent();
